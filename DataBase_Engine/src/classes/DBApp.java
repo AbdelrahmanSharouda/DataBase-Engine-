883aa2272d4791dd156de17825e02b985013e010
mohaned mashaly <30902228+12mohaned@users.noreply.github.com>
2020-02-27 00:10:18 +0200

    Update DBApp.java

--- DataBase_Engine/src/classes/DBApp.java
+++ DataBase_Engine/src/classes/DBApp.java
@@ -1,14 +1,17 @@
 package classes;
 import java.io.FileNotFoundException;
 import java.util.*;
+import classes.Table;
+@SuppressWarnings("unused")
 public class DBApp {
-public void init(){
-	
+static Table Current_Table;
+public static void init(String strTableName , String ClusteringKey, Hashtable<String,Object> ColumnNameType) throws Exception{
+Current_Table = new Table(strTableName, ClusteringKey, ColumnNameType);	
 } 
 // this does whatever initialization you would like // or leave it empty if there is no code you want to
 // execute at application startup
-public static void createTable(String strTableName,String strClusteringKeyColumn,Hashtable<String,Object> htblColNameType) throws FileNotFoundException {
-//Table T = new Table(strTableName, strClusteringKeyColumn, htblColNameType);
+public static void createTable(String strTableName,String strClusteringKeyColumn,Hashtable<String,Object> htblColNameType) throws Exception {
+init(strTableName, strClusteringKeyColumn,htblColNameType);
 }
 //throws DBAppException
 public void createBTreeIndex(String strTableName,String strColName) 
@@ -17,14 +20,13 @@ public void createBTreeIndex(String strTableName,String strColName)
 //throws DBAppException
 public void createRTreeIndex(String strTableName,String strColName) 
 {
-
 }
 //throws DBAppException
-public void insertIntoTable(String strTableName, Hashtable<String,Object> htblColNameValue) 
+public void insertIntoTable(String strTableName, Hashtable<String,Object> htblColNameValue)throws Exception
 {
-	
+Current_Table.insert_into_table(strTableName,htblColNameValue);
 }
-//                                    throws DBAppException
+//throws DBAppException
 public void updateTable(String strTableName, String strKey,Hashtable<String,Object> htblColNameValue) 
 {
 	} 
@@ -34,23 +36,34 @@ public void deleteFromTable(String strTableName, Hashtable<String,Object> htblCo
 	
 }
 //throws DBAppException
-//public Iterator selectFromTable(SQLTerm[] arrSQLTerms, String[] strarrOperators) {
-//	
-//}
 //throws DBAppException
-public static void main(String[]args) throws FileNotFoundException 
+
+@SuppressWarnings({ "unchecked", "static-access" })
+public static void main(String[]args) throws Exception 
 {
-	DBApp dbApp = new DBApp();
-String TA = "TA";	
-Hashtable htblColNameType_1 = new Hashtable();
-htblColNameType_1.put("id", "java.lang.Integer");
-htblColNameType_1.put("name", "java.lang.String");
-htblColNameType_1.put("Job-Type", "java.lang.double");
-
-
-
-dbApp.createTable(TA,"SSN",htblColNameType_1);
-//dbApp.createTable(strTableName_2,"id",  htblColNameType_1);
-
+	DBApp dbApp = new DBApp( );
+	String strTableName = "student";
+	Hashtable htblColNameType = new Hashtable( ); 
+	Hashtable htblColNameValue = new Hashtable( );
+	htblColNameType.put("id", "java.lang.Integer");
+	htblColNameType.put("name", "java.lang.String");
+	htblColNameType.put("gpa", "java.lang.double"); 
+	dbApp.createTable( strTableName, "id", htblColNameType );	
+	htblColNameValue.put("id",  2343432 ); 
+	htblColNameValue.put("name", new String("Nour Ahmed" ) );
+	htblColNameValue.put("gpa",  0.15 ); 
+	
+	htblColNameValue.put("id",  2343432 ); 
+	htblColNameValue.put("name", new String("Nour Ahmed" ) );
+	htblColNameValue.put("gpa",  0.15 ); 
+	dbApp.insertIntoTable( strTableName , htblColNameValue );
+	
+}
+@SuppressWarnings("serial")
+class DBAppException extends Exception {
+public DBAppException(String Message) {
+super("DBApp Exception is thrown");	
 }
 }
+}
+
